631b028689f18603c6a413ad8b7e2eaecbd1d238
samuelsamosir27 <45782947+samuelsamosir27@users.noreply.github.com>
2021-11-09 03:23:50 +0700

    fix: adjust feature

--- featureExtraction/tf-idf/tf_idf.py
+++ featureExtraction/tf-idf/tf_idf.py
@@ -1,17 +1,11 @@
 import numpy as np
 import pandas as pd
-import matplotlib
-import seaborn
-import nltk
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
 
 def run_tfidf_tweet(filename):
-    df = pd.read_csv(filename, delimiter=";;", low_memory=False, header=)
+    df = pd.read_csv(filename, sep=";;;", engine='python')
 
-    x_train, x_test, y_train, y_test = train_test_split(df['tweet'], df['label'], test_size=0.25)
-    print("Train: ", x_train.shape, y_train.shape, "\nTest: ", x_test.shape, y_test.shape)
+    vectorizer = TfidfVectorizer()
+    tfidf_feature = vectorizer.fit_transform(df['teks'].tolist())
 
-    tfidfconverter = TfidfVectorizer(max_features=2500 ,min_df=3, max_df=0.75)
-    tfidf_x_train = tfidfconverter.fit_transform(x_train)
-    tfidf_x_test = tfidfconverter.transform(x_test)
+    return tfidf_feature
--- featureExtraction/tf/tf.py
+++ featureExtraction/tf/tf.py
@@ -3,8 +3,8 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 
 def tf(filepath, n):
-    df = pd.read_csv(filepath, sep="\;\;\;", engine="python")
-    cv = CountVectorizer(ngram_range=(1, 1))
+    df = pd.read_csv(filepath, sep=";;;", engine="python")
+    cv = CountVectorizer(ngram_range=(1, n))
     tf_matrix = cv.fit_transform(df["teks"].tolist())
 
     return tf_matrix
