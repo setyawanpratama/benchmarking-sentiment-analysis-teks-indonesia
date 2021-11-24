6571b6eb6239c4ca422cf5d6bebb037294746ddb
samuelsamosir27 <45782947+samuelsamosir27@users.noreply.github.com>
2021-11-25 02:54:04 +0700

    feat: added uppercase feature

--- featureExtraction/orthography/orthography.py
+++ featureExtraction/orthography/orthography.py
@@ -11,7 +11,8 @@ def run_ortografi(filepath):
         char_len = len(tweet)
         word_len = len(tweet.split(" "))
         symbol = sum((1 for c in tweet if not c.isalnum()))
-        orto_feat = [char_len, word_len, symbol]
+        upper = sum((1 for c in tweet if c.isupper()))
+        orto_feat = [char_len, word_len, symbol, upper]
         all_orto_feat.append(orto_feat)
 
     return np.array(all_orto_feat), features
